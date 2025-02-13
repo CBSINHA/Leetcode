d7ad48b9b6bbbaafc81c1da58892e3ad67c1a94d
Shashank Sinha <shashanksinha590@gmail.com>
2025-02-13 21:41:22 +0530

    Time: 2 ms (11.6%), Space: 41.7 MB (5.66%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int reverse(int x) {
+        try{
+        boolean negative=false;
+        if(x<0){x=x*-1;
+        negative=true;}
+        StringBuilder sb=new StringBuilder();
+        sb.append(x);
+        int left=0,right=sb.length()-1;
+        while(left<right){
+            char temp=sb.charAt(left);
+            sb.setCharAt(left,sb.charAt(right));
+            sb.setCharAt(right,temp);
+            left++;
+            right--;
+        }
+        x=Integer.valueOf(sb.toString());
+        if(negative==true)return x*-1;
+        else
+        return x;
+        }
+        catch(Exception e){
+            return 0;
+        }
+    }
+}
