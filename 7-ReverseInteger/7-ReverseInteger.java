ea7ce67cff9896b5e5078f9c2b93c02ba70891ee
Shashank Sinha <shashanksinha590@gmail.com>
2025-06-26 22:42:56 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.java
@@ -0,0 +1,28 @@
+// Last updated: 6/26/2025, 10:42:53 PM
+class Solution {
+    public int reverse(int x) {
+        try{
+        boolean negative=false;
+        if(x<0){x=x*-1;
+        negative=true;}
+        StringBuilder sb=new StringBuilder();
+        sb.append(x);
+        sb.reverse();
+        // int left=0,right=sb.length()-1;
+        // while(left<right){
+        //     char temp=sb.charAt(left);
+        //     sb.setCharAt(left,sb.charAt(right));
+        //     sb.setCharAt(right,temp);
+        //     left++;
+        //     right--;
+        // }
+        x=Integer.valueOf(sb.toString());
+        if(negative==true)return x*-1;
+        else
+        return x;
+        }
+        catch(Exception e){
+            return 0;
+        }
+    }
+}
