e957eb01f3f16c271fe684d5826d40ace8ff027c
Shashank Sinha <shashanksinha590@gmail.com>
2025-12-10 19:49:08 +0530

    File updated at 10/12/2025, 7:49:07 pm

--- 1295-FindNumberswithEvenNumberofDigits/1295-FindNumberswithEvenNumberofDigits.java
+++ 1295-FindNumberswithEvenNumberofDigits/1295-FindNumberswithEvenNumberofDigits.java
@@ -1,10 +1,14 @@
-// Last updated: 10/12/2025, 7:24:46 pm
+// Last updated: 10/12/2025, 7:49:07 pm
 1class Solution {
-2    public int findNumbers(int[] nums) {
-3        int count=0;
-4        for(int i:nums){
-5            if(String.valueOf(i).length()%2==0) count++;
-6        }
-7        return count;
-8    }
-9}
+2    public int maximumWealth(int[][] accounts) {
+3        int max=Integer.MIN_VALUE;
+4        for(int i=0;i<accounts.length;i++){
+5            int sum=0;
+6            for(int j=0;j<accounts[i].length;j++){
+7                sum+=accounts[i][j];
+8            }
+9            if(sum>max)max=sum;
+10        }
+11        return max;
+12    }
+13}
